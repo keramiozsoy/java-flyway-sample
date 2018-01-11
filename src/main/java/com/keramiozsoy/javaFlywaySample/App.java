fb79b9a1ee28e01060f201ace9d3485549017bc4
Kerami Ozsoy <keramiozsoy@hotmail.com>
2018-01-11 20:15:20 +0200

    Now everything is OK :)

--- src/main/java/com/keramiozsoy/javaFlywaySample/App.java
+++ src/main/java/com/keramiozsoy/javaFlywaySample/App.java
@@ -1,13 +1,36 @@
 package com.keramiozsoy.javaFlywaySample;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
- * Hello world!
- *
+ * 
+ * @author user
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	static Connection c = null;
+
+	public static void main(String[] args) {
+
+		try {
+			c = JDBCUtil.OpenJdbcConnection(
+					//
+					"org.apache.derby.jdbc.EmbeddedDriver", //
+					"jdbc:derby:src/main/resources/testDerbyDB;create=true",
+					"", "");
+		} catch (SQLException e1) {
+			System.out.println("Driver problem");
+			e1.printStackTrace();
+		}
+
+		FlywayUtil.initialize();
+
+		JDBCUtil.getAllDatas(c);
+		
+		JDBCUtil.flwvayInfo(c);
+		
+		JDBCUtil.closeConnection(c);
+	}
 }
